f90532202341f934bb9e2425723d6abc5c136e6a
05Waleed <chw77732@gmail.com>
2024-08-25 23:43:13 +0200

    create launch manager to check if the view controller is already shown or not

---
+++ SPT Mobile/LottieAnimations/LaunchManager.swift
@@ -0,0 +1,29 @@
+//
+//  LaunchManager.swift
+//  SPT Mobile
+//
+//  Created by Muhammad Waleed on 25.08.2024.
+//
+
+import Foundation
+
+class LaunchManager {
+    
+    private let launchKey: String
+    
+    init(launchKey: String) {
+        self.launchKey = launchKey
+    }
+    
+    /// Checks if the view controller associated with this key has been shown before.
+    func isFirstLaunch() -> Bool {
+        return !UserDefaults.standard.bool(forKey: launchKey)
+    }
+    
+    /// Sets the flag indicating that the view controller has been shown.
+    func setFirstLaunchFlag() {
+        UserDefaults.standard.set(true, forKey: launchKey)
+        UserDefaults.standard.synchronize() // Ensure the data is written to disk.
+    }
+}
+
