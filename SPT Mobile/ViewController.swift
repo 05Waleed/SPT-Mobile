dbedebe89208319f14a201ce0b3529e80120423a
Waleed <139551937+05Waleed@users.noreply.github.com>
2024-08-25 19:37:30 +0200

    Add files via upload
    Project Added

---
+++ SPT Mobile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SPT Mobile
+//
+//  Created by Muhammad Waleed on 25.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
