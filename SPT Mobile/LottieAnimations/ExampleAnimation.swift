c5c1ec3eed7910186e0c8227a5a5d72a2e0816cf
05Waleed <chw77732@gmail.com>
2024-08-25 19:44:35 +0200

    create folders to manager code files

---
+++ SPT Mobile/LottieAnimations/ExampleAnimation.swift
@@ -0,0 +1,8 @@
+//
+//  ExampleAnimation.swift
+//  SPT Mobile
+//
+//  Created by Muhammad Waleed on 25.08.2024.
+//
+
+import Foundation
---
+++ SPT Mobile/Managers/ExampleManager.swift
@@ -0,0 +1,8 @@
+//
+//  ExampleManager.swift
+//  SPT Mobile
+//
+//  Created by Muhammad Waleed on 25.08.2024.
+//
+
+import Foundation
