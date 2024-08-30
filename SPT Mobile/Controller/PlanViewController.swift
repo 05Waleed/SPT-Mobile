edbe0ccd1cbfd27acc7cf8dd12beb29c19f00fe3
05Waleed <chw77732@gmail.com>
2024-08-30 15:48:21 +0200

    create plan view controller

---
+++ SPT Mobile/Controller/PlanViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PlanViewController.swift
+//  SPT Mobile
+//
+//  Created by Muhammad Waleed on 30.08.2024.
+//
+
+import UIKit
+
+class PlanViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
