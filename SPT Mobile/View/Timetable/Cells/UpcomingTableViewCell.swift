1aacfee2ea483c94710c2e154e724b8f40657f47
05Waleed <chw77732@gmail.com>
2024-09-03 00:48:44 +0200

    unknow commit in timatableView

--- SPT Mobile/View/Timetable/Cells/UpcomingTableViewCell.swift
+++ SPT Mobile/View/Timetable/Cells/UpcomingTableViewCell.swift
@@ -20,7 +20,22 @@ class UpcomingTableViewCell: UITableViewCell {
     }
     
     func updateData(from: PlanViewData, indexPath: IndexPath) {
-       
+        directionLbl.text = from.legs?[indexPath.row].exit?.name
+        
+        timeLbl.text = HelperFunctions.getTime(from: "\(from.legs?[indexPath.row].departure ?? "")")
+        
+        lineLbl.text = "\(from.legs?[indexPath.row].g ?? "") \(from.legs?[indexPath.row].l ?? "")"        
     }
     
+    func updateImage(from: PlanViewData, indexPath: IndexPath) {
+        if from.legs?[indexPath.row].type == "bus" {
+            vehicleImg.image = UIImage(named: "bus 1")
+        } else if from.legs?[indexPath.row].type == "strain" {
+            vehicleImg.image = UIImage(named: "strain")
+        } else if from.legs?[indexPath.row].type == "tram" {
+            vehicleImg.image = UIImage(named: "tram")
+        } else {
+            vehicleImg.image = UIImage(named: "dot")
+        }
+    }
 }
